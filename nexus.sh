381610fb65d8a40bb26d49cda5c29744bfcdee29
Sairam <pvsairam@hotmail.com>
2024-10-27 03:28:20 +0530

    Update nexus.sh

--- nexus.sh
+++ nexus.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -s https://raw.githubusercontent.com/zunxbt/logo/main/logo.sh | bash
+curl -s https://raw.githubusercontent.com/pvsairam/logo/refs/heads/main/logo.sh | bash
 sleep 5
 
 BOLD=$(tput bold)
